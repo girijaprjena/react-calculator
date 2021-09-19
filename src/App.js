b7716ffd03999d17e908d70a36289c720625b90f
Girija <girija248.nalanda@gmail.com>
2021-09-19 23:59:32 +0530

    some ui changes done

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends Component {
             <Button handleClick={this.addToInput}>-</Button>
           </div>
           <div className="row">
-            <ClearButton handleClear={() => this.setState({input:""})}>Clear</ClearButton>
+            <ClearButton handleClear={() => this.setState({input:""})}>CLEAR</ClearButton>
           </div>
         </div>
       </div>
